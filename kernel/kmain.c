ae9381be4f1710a1c4a7a27ca333e57e6c732d7e
kagantmr <mkagantemur@gmail.com>
2026-03-17 03:00:35 +0300

    version string

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -4,11 +4,13 @@
 #include "string.h"
 #include "vga/vga.h"
 #include "splash.h"
+#include "core/panic.h"
+#include "version.h"
 
 _Noreturn void kmain(void) {
     vga_clear();
     kprintf_init(vga_putc);
     splash();
-    kprintf("Welcome to Zuzu386!\n");
+    kprintf("Welcome to Zuzu386 (version %s)!\n", Z386_VERSION);
     while (1);
 }
---
+++ kernel/version.h
@@ -0,0 +1,6 @@
+#ifndef Z386_VERSION_H
+#define Z386_VERSION_H
+
+#define Z386_VERSION "0.1.0"
+
+#endif
